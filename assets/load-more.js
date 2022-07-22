abb8fbe3ce508d0b8c032e00c8b83e3d5d79adb8
khawarcp <khawaraneescp@gmail.com>
2022-07-22 15:14:47 +0500

    load more button js styling

--- assets/load-more.js
+++ assets/load-more.js
@@ -1,15 +1,25 @@
 var products_on_page = $('.products-on-page');
 var next_url = products_on_page.data('next-url');
+var load_more_btn = $('.load-more__btn');
+var load_more_spinner = $('.load-more__spinner');
+
 function shopMore(){
     $.ajax({
     url : next_url,
     type : 'GET',
-    dataType : 'html'
+    dataType : 'html',
+    beforeSend: function(){
+        load_more_spinner.show();
+        load_more_btn.hide();
+    }
  }).done(function(next_page){
+    load_more_spinner.hide();
     var new_products = $(next_page).find('.products-on-page');
     var new_url = new_products.data('next-url');
-    if(new_url)
-        next_url = new_url;
+    if(new_url){
+        load_more_btn.show();
+    }
+    next_url = new_url;
     products_on_page.append(new_products.html());
  })
 }
