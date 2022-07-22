a47520506c4f6b6b9627514e5f7ea1e6302d7b7a
khawarcp <khawaraneescp@gmail.com>
2022-07-22 10:48:37 +0500

    variant selection homepage and collection page

--- assets/custom.js
+++ assets/custom.js
@@ -6,3 +6,20 @@ $(document).ready(function(){
     $(this).toggleClass("active");
   });
 })
+
+
+// select variants at homepage and collection page
+var varVal;
+var replaceId;
+$('.input-price-row').find('input[type=radio]').on('change', function(){
+    varVal = $(this).val();
+    $(this).closest('form').find('.variants-list-items').each(function(){
+        var masterVal = $(this).text();
+        
+        if(masterVal.includes(varVal)){
+            replaceId = $(this).data('variantid');
+            $(this).closest('form').find('input[name=id]').val(replaceId);
+            
+        }
+    })
+});
